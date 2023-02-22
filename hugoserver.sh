f6162ce0d1a760c67ab59c022ca0c2809a644760
Fernando Ike <fike@maburix.com>
2023-02-22 07:54:26 -0300

    review text

--- hugoserver.sh
+++ hugoserver.sh
@@ -12,4 +12,4 @@
 docker stop hugo-server
 docker rm   hugo-server
 #docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_THEME=devopsdays-theme -e HUGO_BASEURL="http://localhost:1313" --name hugo-server --entrypoint "" cibuilds/hugo:0.67.0 hugo server --watch --bind ""
-docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_BASEURL="http://localhost:1313" --name hugo-server --entrypoint "" cibuilds/hugo:0.67.0 hugo server --watch --bind ""
+docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_BASEURL=hyde-x --name hugo-server --entrypoint "" cibuilds/hugo:0.67.0 hugo server --watch --bind ""
